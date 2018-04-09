306e44a876327b95e020484a952228d3b89d1992
UrielBicharra <ugb@icomp.ufam.edu.br>
2018-04-09 09:40:41 -0400

    Exercício JS

---
+++ JS/JS1/script.js
@@ -0,0 +1,38 @@
+function tableCreate(){
+	var body = document.body;
+	var tbl = document.createElement('table');
+	tbl.style.width  = '100px';
+	tbl.style.border = '1px solid black';
+
+	var thead = tbl.createTHead();
+	var trH = thead.insertRow();
+	var tdH = trH.insertCell();
+	tdH.appendChild(document.createTextNode('head'));
+	tdH.style.border = '1px solid black';
+	tdH.setAttribute('colSpan','2');
+
+	for(var i = 1; i <= 10; i++){
+        var tr = tbl.insertRow();
+        for(var j = 0; j < 2; j++){
+            var td = tr.insertCell();
+			if(j == 0){
+				td.appendChild(document.createTextNode(""+ num + "x" + i));
+			} else {
+            td.appendChild(document.createTextNode("" + (num * i)));
+			}
+			td.style.border = '1px solid black';
+        }
+    }
+    body.appendChild(tbl);
+}
+
+var num = 1;
+while (num <= 10) {
+	tableCreate();
+	num++;
+}
+/*
+tableCreate();
+tableCreate();
+tableCreate();
+*/
