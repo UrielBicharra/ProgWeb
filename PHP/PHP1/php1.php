673470908501dda74d9811724507ec7606eae221
UrielBicharra <ugb@icomp.ufam.edu.br>
2018-05-21 09:20:51 -0400

    PHP exercício 1
    add textarea

--- PHP/PHP1/php1.php
+++ PHP/PHP1/php1.php
@@ -4,6 +4,7 @@
       Nome  <?php echo $_POST["nome"]; ?><br>
       Email <?php echo $_POST["mail"]; ?><br>
       Site  <?php echo $_POST["site"]; ?><br>
+      Mensagem <<?php echo $_POST["msg"]; ?><br>
 
 </body>
 </html>
