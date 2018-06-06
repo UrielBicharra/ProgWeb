99c1f5a029c7c2f84fd0e34361e6d6b4642774ff
UrielBicharra <ugb@icomp.ufam.edu.br>
2018-06-06 09:16:40 -0400

    added cruds and models

--- skifree_yii/advanced/frontend/views/layouts/main.php
+++ skifree_yii/advanced/frontend/views/layouts/main.php
@@ -39,6 +39,8 @@ AppAsset::register($this);
         ['label' => 'Home', 'url' => ['/site/index']],
         ['label' => 'About', 'url' => ['/site/about']],
         ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => 'Usuários', 'url' => ['/user/index']],
+        ['label' => 'Cursos', 'url' => ['/curso/index']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
